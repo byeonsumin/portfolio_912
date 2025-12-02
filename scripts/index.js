a3f1f6047a7fb9f4078ab53e8f42d1d831ba297a
soomin3205 <soomin3205@naver.com>
2025-12-02 12:43:43 +0900

    912 프로젝트 작업

--- scripts/index.js
+++ scripts/index.js
@@ -15,12 +15,35 @@ var swiper = new Swiper(".best_seller_list", {
 });
 
 
+
 /* 퍼퓸 카테고리 */
 var swiper = new Swiper(".perfume_swiper", {
     slidesPerView: 'auto',
     centeredSlides: true,       // 가운데 정렬
     spaceBetween: 20,           // 카드 간격
     initialSlide: 2,            // 시작 슬라이드 (원하면 변경 가능)
-    loop: false,
+    loop: true,
     centeredSlidesBounds: true,
+});
+
+
+
+
+/* 기프트 */
+const giftSwiper = new Swiper("#gift_swiper", {
+    slidesPerView:5,
+    centeredSlides: true,
+    spaceBetween: 0,
+    loop:false,
+    initialSlide:2,   //가운데(중앙) 슬라이드에서 시작
+    navigation: {
+        nextEl: "#gift_swiper .swiper-button-next",
+        prevEl: "#gift_swiper .swiper-button-prev"
+    },
+});
+// 슬라이드 클릭 시 해당 슬라이드로 이동
+document.querySelectorAll('#gift_swiper .swiper-slide').forEach((slide, index) => {
+    slide.addEventListener('click', () => {
+        giftSwiper.slideTo(index);  // 해당 index로 이동
+    });
 });
